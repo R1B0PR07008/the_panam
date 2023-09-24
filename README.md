9e67fd0a8704e833d1295fc7c501f2f026929cf1
R1B0PR07008 <Matirilop@gmail.com>
2023-09-24 06:29:18 -0600

    added scroll-able boxes
    Also changed home.html to index.html for compatibility purposes as well as changed the file paths for the same purpose.

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # the_panam
-This is the code for the wbsite of the school news paper: The Panam. 
+This is the code for the website of the school news paper: The Panam. 
 
 This code was made by: 
   Matias Riboldi 10-A
   Saul Soto 11-B
   Maria Ignacia Alcayaga 10-A
 
+main VSC extensions used are the following: 
+  better comments
+  live server(live reset)
+  prettier
+
+little things to note: 
+  The home page must be named index.html
+  All file paths must start with '/public/the_panam/' for the server to find the file.
--- site.js
+++ site.js
@@ -1,2 +1,26 @@
 console.log('Hello! We see you are looking through our code >:)');
 
+
+//* function to create main-sec1-div1 divs
+span = ['<div class="main-expandableDiv"></div>'];
+span_out = '';
+
+for (i = 1;i<5;i++) {
+    span.push('<div class="main-expandableDiv"></div>');
+}
+
+console.log(span);
+span_out = span.join(' ');
+document.getElementById('main-sec1-segments').innerHTML = span_out;
+
+//* function to create main-sec2-div1 divs
+span2 = ['<div class="main-expandableDiv-2"></div>'];
+span_out2 = '';
+
+for (i = 1;i<5;i++) {
+    span2.push('<div class="main-expandableDiv-2"></div>');
+}
+
+console.log(span2);
+span_out2 = span2.join(' ');
+document.getElementById('main-sec2-segments').innerHTML = span_out2;
